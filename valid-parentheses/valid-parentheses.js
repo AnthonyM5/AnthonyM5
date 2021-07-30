a1f721499b1c5a5f43ea87aea5f4165a753f1f25
Anthony Mai <anthony.mai530@gmail.com>
2021-07-29 22:49:48 -0400

    Time: 76 ms (67.17%), Space: 39.2 MB (51.41%) - LeetHub

---
+++ valid-parentheses/valid-parentheses.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    
+    let brackets = {
+        "(": ")",
+        "{": "}",
+        "[": "]"
+    }
+    
+    let stack = [];
+    
+    for (let i = 0; i < s.length; i++) {
+        if (brackets[s[i]] !== undefined) {
+            stack.push(brackets[s[i]])
+        } else {
+            if (stack.pop() !== s[i]) {
+                return false
+            }
+        }
+    }
+    return stack.length > 0 ? false : true
+};
