508dbc3187c1fdbfc9938da5ed70373b633bb539
Anthony Mai <anthony.mai530@gmail.com>
2022-04-30 14:27:47 -0400

    Time: 94 ms, Memory: 44.6 MB - LeetHub

--- merge-two-sorted-lists/merge-two-sorted-lists.js
+++ merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -6,21 +6,20 @@
  * }
  */
 /**
- * @param {ListNode} l1
- * @param {ListNode} l2
+ * @param {ListNode} list1
+ * @param {ListNode} list2
  * @return {ListNode}
  */
-var mergeTwoLists = function(l1, l2) {
-    if (!l2) return l1 
-    if (!l1) return l2
+const mergeTwoLists = (list1, list2) => {
+    if (!list2) return list1 
+    if (!list1) return list2
     
-    if (l1.val > l2.val) {
-        l2.next = mergeTwoLists(l1, l2.next)
-        return l2
+    if (list1.val > list2.val) {
+        list2.next = mergeTwoLists(list1, list2.next)
+        return list2
     }
     else {
-        l1.next = mergeTwoLists(l1.next, l2)
-        return l1
+        list1.next = mergeTwoLists(list1.next, list2)
+        return list1
     }
-    
 };
