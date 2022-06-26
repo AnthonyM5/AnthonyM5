e84d193c1b89e0bda0ddbaca80f57211574a92b2
Anthony Mai <anthony.mai530@gmail.com>
2022-06-26 19:20:17 -0400

    Time: 122 ms (35.91%), Space: 42.3 MB (84.97%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+
+
+const isIsomorphic = (s, t) => {
+    function isomorphic(s,t) {
+        let seen = {}
+        for(let i = 0; i < s.length; i++) {
+            if(s[i] in seen) {
+                if(seen[s[i]] !== t[i]) {
+                    return false
+                }
+            } else {
+                seen[s[i]] = t[i]
+            }
+        }
+        return true
+    }
+    return isomorphic(s,t) && isomorphic(t,s)
+};
