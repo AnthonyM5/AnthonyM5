a3911fbdfe1d6fcd019a15b83241ed4c2961fee4
Anthony Mai <anthony.mai530@gmail.com>
2022-07-21 22:20:00 -0400

    Time: 80 ms (86.80%), Space: 42.7 MB (51.46%) - LeetHub

---
+++ 1-two-sum/1-two-sum.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = (nums, target) => {
+    let map = {};
+    for (let i = 0; i < nums.length; i++) {
+        let n = nums[i]
+
+        if(map[target-n] >= 0){
+            return [map[target-n],i]
+        } 
+        else {
+            map[n] = i
+        }
+    }
+};
