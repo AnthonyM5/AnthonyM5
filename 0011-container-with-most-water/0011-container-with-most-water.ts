9e81cb37e558255415f3196ee79b5fdb0b4513ce
Anthony Mai <anthony.mai530@gmail.com>
2026-08-01 15:54:46 -0400

    Time: 0 ms (100%), Space: 62.9 MB (93.81%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.ts
+++ 0011-container-with-most-water/0011-container-with-most-water.ts
@@ -1,13 +1,16 @@
 function maxArea(height: number[]): number {
-    let maxArea = 0;
     let left = 0;
     let best = 0;
     let right = height.length - 1;
 
+    // Sliding window, start from left pointer to right pointer until they meet at the middle
     while (left < right) {
+        // take the lower of the two heights 
         const lh = Math.min(height[left], height[right])
-        best = Math.max(best, lh * (right - left))
 
+        // keep track of the best area: lh * the space between the two pointers) 
+        best = Math.max(best, lh * (right - left))
+        // if the left pointer height is lower than the right, we can shrink the window from the left, if the height on the right is higher, we can shrink the window from the right.
         if (height[left] < height[right]) {
             left++
         } else {
