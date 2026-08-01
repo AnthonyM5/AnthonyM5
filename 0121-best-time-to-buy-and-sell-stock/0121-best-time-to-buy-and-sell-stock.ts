e21e627485728c83b3546ee4b8bb5d46d1b030ad
Anthony Mai <anthony.mai530@gmail.com>
2026-08-01 17:45:45 -0400

    Time: 2 ms (78.18%), Space: 65.3 MB (87.73%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
@@ -0,0 +1,10 @@
+function maxProfit(prices: number[]): number {
+    let profit = 0;
+    let min = prices[0];
+
+    for (let i = 0; i < prices.length; i++) {
+        min = Math.min(min, prices[i])
+        profit = Math.max(profit, prices[i] - min)
+    }
+    return profit
+};
