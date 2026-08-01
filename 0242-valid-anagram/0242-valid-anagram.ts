338162784d6760220c59fdc9bf7d28bb0f98ddc6
Anthony Mai <anthony.mai530@gmail.com>
2026-08-01 12:35:23 -0400

    Time: 20 ms (28.05%), Space: 61.7 MB (15.32%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.ts
+++ 0242-valid-anagram/0242-valid-anagram.ts
@@ -1,11 +1,18 @@
 function isAnagram(s: string, t: string): boolean {
   if (s.length !== t.length) return false;
   const counts = new Map<string, number>();
-  for (const char of s) counts.set(char, (counts.get(char) ?? 0) + 1);
-  for (const char of t) {
-    const c = counts.get(char) ?? 0;
-    if (c === 0) return false;
-    counts.set(char, c - 1);
+
+  for (let char of s) {
+    counts.set(char, (counts.get(char) ?? 0) + 1)
   }
-  return true;
+
+  for (let char of t) {
+    const count = counts.get(char) ?? 0
+    if (count === 0) {
+        return false
+    } else {
+        counts.set(char, (counts.get(char) - 1))
+    }
+  }
+  return true
 }
